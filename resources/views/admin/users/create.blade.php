a639e60398ecb1043896daf6c573d3ce4fbce69f
Luiz Pedro <luizpsmoreira@gmail.com>
2020-01-08 19:55:15 -0300

    Usuários: Error no Input

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -11,7 +11,7 @@
 @if ($errors->any())
     <div class="alert alert-danger">
         <ul>
-            <h4>Ocorreu um erro.</h4>
+            <h5> <i class="icon fas fa-ban"></i> Ocorreu um erro.</h5>
             @foreach ($errors->all() as $error)
                 <li>{{$error}}</li>
             @endforeach
@@ -19,49 +19,42 @@
     </div>
 @endif
 
-
-<form action="{{ route('users.store') }}" method="post" class="form-horizontal">
-    @csrf
-    <div class="form-group">
-        <div class="row">
-            <label for="name" class="col-sm-2 control-label">Nome Completo</label>
-            <div class="col-sm-10">
-            <input type="text" name="name" id="name" class="form-control" value="{{old('name')}}">
+<div class="card">
+    <div class="card-body">
+        <form action="{{ route('users.store') }}" method="post" class="form-horizontal">
+            @csrf
+            <div class="form-group row">
+                <label for="name" class="col-sm-2 col-form-label">Nome Completo</label>
+                <div class="col-sm-10">
+                    <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="row">
-            <label for="email" class="col-sm-2 control-label">E-mail</label>
-            <div class="col-sm-10">
-                <input type="email" name="email" id="email" class="form-control" value="{{old('email')}}">
+            <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">E-mail</label>
+                <div class="col-sm-10">
+                    <input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror" value="{{old('email')}}">
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="row">
-            <label for="password" class="col-sm-2 control-label">Senha</label>
-            <div class="col-sm-10">
-                <input type="password" name="password" id="password" class="form-control">
+            <div class="form-group row">
+                <label for="password" class="col-sm-2 col-form-label">Senha</label>
+                <div class="col-sm-10">
+                    <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror">
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="row">
-            <label for="password_confirmation" class="col-sm-2 control-label">Confirmar Senha</label>
-            <div class="col-sm-10">
-                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
+            <div class="form-group row">
+                <label for="password_confirmation" class="col-sm-2 col-form-label">Confirmar Senha</label>
+                <div class="col-sm-10">
+                    <input type="password" name="password_confirmation" id="password_confirmation" class="form-control @error('password') is-invalid @enderror">
+                </div>
             </div>
-        </div>
-    </div>
-        <div class="form-group">
-        <div class="row">
-            <label class="col-sm-2 control-label"></label>
-            <div class="col-sm-10">
-                <input type="submit" value="Cadastrar" class="btn btn-success">
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label"></label>
+                <div class="col-sm-10">
+                    <input type="submit" value="Cadastrar" class="btn btn-success">
+                </div>
             </div>
-        </div>
+        </form>
     </div>
-</form>
+</div>
 
 @endsection
