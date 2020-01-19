f436b053d070126364e6f079086a6dc3f51c63e7
Luiz Pedro <luizpsmoreira@gmail.com>
2020-01-19 18:58:48 -0300

    Página: TinyMCE no Adicionar

--- resources/views/admin/pages/create.blade.php
+++ resources/views/admin/pages/create.blade.php
@@ -3,20 +3,20 @@
 @section('title', 'Nova Página')
 
 @section('content_header')
-    <h1>Nova Página</h1>
+<h1>Nova Página</h1>
 @endsection
 
 @section('content')
 
 @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            <h5> <i class="icon fas fa-ban"></i> Ocorreu um erro.</h5>
-            @foreach ($errors->all() as $error)
-                <li>{{$error}}</li>
-            @endforeach
-        </ul>
-    </div>
+<div class="alert alert-danger">
+    <ul>
+        <h5> <i class="icon fas fa-ban"></i> Ocorreu um erro.</h5>
+        @foreach ($errors->all() as $error)
+        <li>{{$error}}</li>
+        @endforeach
+    </ul>
+</div>
 @endif
 
 <div class="card">
@@ -26,13 +26,14 @@
             <div class="form-group row">
                 <label for="name" class="col-sm-2 col-form-label">Título</label>
                 <div class="col-sm-10">
-                    <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" value="{{old('title')}}">
+                    <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror"
+                        value="{{old('title')}}">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="email" class="col-sm-2 col-form-label">Corpo</label>
                 <div class="col-sm-10">
-                    <textarea name="body" class="form-control">{{old('body')}}</textarea>
+                    <textarea name="body" class="form-control bodyfield">{{old('body')}}</textarea>
                 </div>
             </div>
             <div class="form-group row">
@@ -44,5 +45,22 @@
         </form>
     </div>
 </div>
+
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    tinymce.init({
+    selector:'textarea.bodyfield',
+    height:300,
+    menubar:false,
+    plugins:['link','table', 'image', 'autoresize', 'lists'],
+    toolbar:'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | table | link image | bullist numlist',
+    content_css:[
+        '{{asset('assets/css/content.css')}}'
+    ],
+    images_upload_url:'{{route('imageupload')}}',
+    images_upload_credentials:true,
+    convert_urls:false
+});
+</script>
 
 @endsection
